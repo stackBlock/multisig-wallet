1c04de44b6282a57af631401b0099eb35bb9a574
stackblock <stackchain@gmail.com>
2020-05-25 12:19:03 -0400

    errors

---
+++ test/wallet.js
@@ -0,0 +1,27 @@
+const Wallet = artifacts.require('Wallet');
+
+contract('Wallet', (accounts) => {
+    let wallet;
+    beforeEach(async () => {
+        wallet =    await Wallet.new([
+                                    accounts[0], 
+                                    accounts[1], 
+                                    accounts[2]
+                                    ], 2);
+                    await web3.eth.sendTransaction({
+                                    from: accounts[0], 
+                                    to: wallet.address, 
+                                    value: 1000
+        });
+
+        it('should have the correct approvers and quorum', async () => {
+            const approvers =   await wallet.getApprovers();
+            const quorum =      await wallet.quorum();
+                assert(approvers.length === 3);
+                assert(approvers[0] === accounts[0]);
+                assert(approvers[1] === accounts[1]);
+                assert(approvers[2] === accounts[2]);
+                assert(quorum.toNumber() === 2); // using BN.js library, we need to use toNumber()
+        })
+    });
+});
